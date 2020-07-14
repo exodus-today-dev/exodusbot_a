e199165066481e67cc99e3b5c58f31fc1c0e5fcf
ArtemS <smetatsc@yandex.ru>
2020-07-14 18:36:09 +0300

    fix bugs and 6.4,6.5,6.9,6.10 done

--- events.py
+++ events.py
@@ -5,7 +5,7 @@ import config
 
 bot = telebot.TeleBot(config.API_TOKEN)
 
-from models import read_exodus_user, read_event, read_intention
+from models import read_exodus_user, read_event, read_intention, update_event
 from models import session, Exodus_Users
 
 
@@ -14,38 +14,37 @@ def invitation_help_orange(event_id):
     user = read_exodus_user(event.from_id)
     bot_text = f"Приглашение помогать {user.first_name} {user.last_name}"
 
-
     keyboard = types.InlineKeyboardMarkup()
-    row=[]
+    row = []
     row.append(types.InlineKeyboardButton('Подробнее', callback_data='orange_invitation-{}'.format(user.telegram_id)))
     keyboard.row(*row)
     bot.send_message(event.to_id, bot_text, reply_markup=keyboard)
     return True
 
-	
+
 def invitation_help_red(event_id):
     event = read_event(event_id)
     user = read_exodus_user(event.from_id)
     bot_text = f"Приглашение помогать {user.first_name} {user.last_name}"
 
-
     keyboard = types.InlineKeyboardMarkup()
-    row=[]
-    row.append(types.InlineKeyboardButton('Подробнее', callback_data='orange_invitation-{}'.format(event.event_id)))
+    row = []
+    row.append(types.InlineKeyboardButton('Подробнее', callback_data='red_invitation-{}'.format(user.telegram_id)))
     keyboard.row(*row)
     bot.send_message(event.to_id, bot_text, reply_markup=keyboard)
     return True
 
-	
+
 def notice_of_intent(event_id):
     """Information about a function.
     """
     event = read_event(event_id)
     user = read_exodus_user(telegram_id=event.from_id)
-    intent = read_intention(event.from_id,event.to_id,1).first()   #create_date
+    intent = read_intention(event.from_id, event.to_id, 1).first()  # create_date
     bot_text = f"{intent.create_date.strftime('%d %B %Y')}/{intent.create_date.strftime('%I:%M%p')}\n\
 Участник {user.first_name} {user.last_name} записал свое намерение помогать вам на сумму: {intent.payment} {event.currency}"
     bot.send_message(event.to_id, bot_text)
+
 
 # 6.4
 def obligation_sended_notice(event_id):
@@ -54,13 +53,13 @@ def obligation_sended_notice(event_id):
     user = read_exodus_user(telegram_id=event.from_id)
     first_name = user.first_name
     last_name = user.last_name
-    intent = read_intention(event.from_id, event.to_id, 12).first() # check status
+    intent = read_intention(event.from_id, event.to_id, 12).first()  # check status
     sum = intent.payment
     currency = intent.currency
 
-    requisites = read_requisites_by_user_id(telegram_id=event.to_id)
-    requisites_name = requisites.name
-    requisites_value = requisites.value
+    # requisites = read_requisites_by_user_id(telegram_id=event.to_id)
+    # requisites_name = requisites.name
+    # requisites_value = requisites.value
 
     message = 'Участник {first_name} {last_name} исполнил ' \
               'обязательства на сумму {sum} {currency}.\n\n' \
@@ -68,28 +67,67 @@ def obligation_sended_notice(event_id):
               'подтвердите получение:'.format(first_name=first_name,
                                               last_name=last_name,
                                               sum=sum, currency=currency,
-                                              req_name=requisites_name,
-                                              req_value=requisites_value)
+                                              req_name='requisites_name',
+                                              req_value='requisites_value')
 
     keyboard = types.InlineKeyboardMarkup()
     row = []
     row.append(types.InlineKeyboardButton('Напомнить позже',
-                                          callback_data='remind_later_{}'))
+                                          callback_data='remind_later_{}'.format(event.event_id)))
     row.append(types.InlineKeyboardButton('Да, я получил',
                                           callback_data='send_confirmation_{}'. \
-                                          format(event.to_id)))
+                                          format(event.event_id)))
     keyboard.row(*row)
 
     bot.send_message(event.to_id, message, reply_markup=keyboard)
     return True
 
 
+# 6.9
 def obligation_recieved_notice(event_id):
-    pass # 6.9
+    event = read_event(event_id)
+    user = read_exodus_user(telegram_id=event.to_id)
+    print(user.first_name)
+    confirmation_of_an_obligation(event.from_id, user.first_name, event.current_payments, event.currency)
+    # update_event(event_id, True)
+
+
+# 6.10
+def reminder_for_6_10(event_id):
+    event = read_event(event_id)
+
+    user = read_exodus_user(telegram_id=event.from_id)
+    first_name = user.first_name
+
+    message = 'Требуется ваше действие!' \
+              'Участник {first_name} исполнил ' \
+              'обязательство на сумму {sum} {currency}.' \
+              'Это произошло болеее 5 дней назад, но вы так и не подтвердили получение средств.\n\n' \
+              'Пожалуйста, проверьте ваши реквизиты и ' \
+              'подтвердите получение:'.format(first_name=first_name,
+                                              sum=event.current_payments, currency=event.currency)
+
+    keyboard = types.InlineKeyboardMarkup()
+    row = []
+    row.append(types.InlineKeyboardButton('Напомнить позже',
+                                          callback_data='6_10_remind_later_{}'.format(event.event_id)))
+
+    row.append(types.InlineKeyboardButton('Да, я получил',
+                                          callback_data='6_10_send_confirmation_{}'. \
+                                          format(event.event_id)))
+
+    row.append(types.InlineKeyboardButton('Нет, я не получил',
+                                          callback_data='6_10_no_send_confirmation_{}'. \
+                                          format(event.event_id)))
+
+    keyboard.row(*row)
+
+    bot.send_message(event.to_id, message, reply_markup=keyboard)
+    return True
 
 
 def obligation_money_requested_notice(event_id):
-    pass # 6.3
+    pass  # 6.3
 
 
 def reminder(event_id):
@@ -105,33 +143,47 @@ def reminder(event_id):
     bot.send_message(event.to_id, message, reply_markup=keyboard)
 
 
-def confirmation_of_an_obligation(message, name: str, amount: int, currency: int) -> None:
+def confirmation_of_an_obligation(chat_id, name: str, amount: int, currency: int) -> None:
     """Information about a function.
     """
-    bot.send_message(message.chat.id, f"Участник {name} подтвердил, что ваше обязательство на сумму {amount} {currency} исполнено.")
+    bot.send_message(chat_id,
+                     f"Участник {name} подтвердил, что ваше обязательство на сумму {amount} {currency} исполнено.")
+
 
 def cancellation_of_intent(message, name: str, amount: int, currency: int) -> None:
     """Information about a function.
     """
-    bot.send_message(message.chat.id, f"Участник {name} отменил свое намерение помогать вам на сумму {amount} {currency}.")
+    bot.send_message(message.chat.id,
+                     f"Участник {name} отменил свое намерение помогать вам на сумму {amount} {currency}.")
 
-def network_member_new_intent(message, newname: str, name: str, amount: int, currency: int, status: str, x: int, y: int, z: int, q: int) -> None:
+
+def network_member_new_intent(message, newname: str, name: str, amount: int, currency: int, status: str, x: int, y: int,
+                              z: int, q: int) -> None:
     """Information about a function.
     """
-    bot.send_message(message.chat.id, f"Новый участник {newname} записал намерение помогать Участнику {name} на сумму {amount} {currency}.\n\nУчастник {name} {status}\nСобрано {x} из {y} {currency}.\nОжидается {z} {currency}.\nВсего участников: {q}.")
+    bot.send_message(message.chat.id,
+                     f"Новый участник {newname} записал намерение помогать Участнику {name} на сумму {amount} {currency}.\n\nУчастник {name} {status}\nСобрано {x} из {y} {currency}.\nОжидается {z} {currency}.\nВсего участников: {q}.")
 
-def cancel_intent_on_network_member(message, newname: str, name: str, amount: int, currency: int, status: str, x: int, y: int, z: int, q: int) -> None:
+
+def cancel_intent_on_network_member(message, newname: str, name: str, amount: int, currency: int, status: str, x: int,
+                                    y: int, z: int, q: int) -> None:
     """Information about a function.
     """
-    bot.send_message(message.chat.id, f"Участник {newname} отменил свое намерение помогать Участнику {name} на сумму {amount} {currency}.\n\nУчастник {name} {status}.\nСобрано {x} из {y} {currency}.\nОжидается {z} {currency}.\nВсего участников: {q}.")
+    bot.send_message(message.chat.id,
+                     f"Участник {newname} отменил свое намерение помогать Участнику {name} на сумму {amount} {currency}.\n\nУчастник {name} {status}.\nСобрано {x} из {y} {currency}.\nОжидается {z} {currency}.\nВсего участников: {q}.")
 
-def new_network_member_commitment(message, newname: str, name: str, amount: int, currency: int, status: str, x: int, y: int, z: int, q: int) -> None:
+
+def new_network_member_commitment(message, newname: str, name: str, amount: int, currency: int, status: str, x: int,
+                                  y: int, z: int, q: int) -> None:
     """Information about a function.
     """
-    bot.send_message(message.chat.id, f"Новый участник {newname} перевел намерение в обязательство перед Участником {name} на сумму {amount} {currency}.\n\nУчастник {name} {status}.\nСобрано {x} из {y} {currency}.\nОжидается {z} {currency}.\nВсего участников: {q}.")
+    bot.send_message(message.chat.id,
+                     f"Новый участник {newname} перевел намерение в обязательство перед Участником {name} на сумму {amount} {currency}.\n\nУчастник {name} {status}.\nСобрано {x} из {y} {currency}.\nОжидается {z} {currency}.\nВсего участников: {q}.")
 
-def network_member_commitment_fulfilled(message, newname: str, name: str, amount: int, currency: int, status: str, x: int, y: int, z: int, q: int) -> None:
+
+def network_member_commitment_fulfilled(message, newname: str, name: str, amount: int, currency: int, status: str,
+                                        x: int, y: int, z: int, q: int) -> None:
     """Information about a function.
     """
-    bot.send_message(message.chat.id, f"Участник {newname} выполнил обязательство перед Участником {name} на сумму {amount} {currency}.\n\nУчастник {Имя} {name}.\nСобрано {x} из {y} {status}.\nОжидается {z} {currency}.\nВсего участников: {q }.")
-
+    bot.send_message(message.chat.id,
+                     f"Участник {newname} выполнил обязательство перед Участником {name} на сумму {amount} {currency}.\n\nУчастник {name}.\nСобрано {x} из {y} {status}.\nОжидается {z} {currency}.\nВсего участников: {q}.")
--- events_worker.py
+++ events_worker.py
@@ -2,12 +2,12 @@
 
 # This is a simple echo bot using the decorator mechanism.
 # It echoes any incoming text messages.
+from datetime import date, timedelta
 
 import telebot
 from telebot import types
 
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-
 
 import time
 
@@ -15,46 +15,64 @@ import config
 
 bot = telebot.TeleBot(config.API_TOKEN)
 
-
-
-
-#--------------------------------- DB ------------------------------
-
+# --------------------------------- DB ------------------------------
 
 
 from models import session, Exodus_Users, Events, update_event
-from events import invitation_help_orange, invitation_help_red, notice_of_intent
-
+from events import invitation_help_orange, invitation_help_red, notice_of_intent, obligation_sended_notice, \
+    obligation_recieved_notice, obligation_money_requested_notice, reminder, reminder_for_6_10
 
 user_dict = {}
 bot.remove_webhook()
 
+# завели списки, чтобы добавить туда event_id отправленных уведомлений
+list_event_id_obligation_sended = []
+list_event_id_obligation_recieved = []
+list_event_id_for_6_10 = []
+
+
 def read():
     all_users = session.query(Exodus_Users).count()
-    all = session.query(Events).filter_by(sent = False)
+    all = session.query(Events).filter_by(sent=False)
     current_date = date.today()
+
     for event in all:
         if event.type == 'orange':
-            update_event(event.event_id,True)
+            update_event(event.event_id, True)
             invitation_help_orange(event.event_id)
         if event.type == 'red':
-            update_event(event.event_id,True)
+            update_event(event.event_id, True)
             invitation_help_red(event.event_id)
         if event.type == 'notice':
-            update_event(event.event_id,True)
+            update_event(event.event_id, True)
             notice_of_intent(event.event_id)
-        if event_type == 'obligation_sended' and (current_date == event.reminder_date or
-                                                  current_date > event.reminder_date):
-            obligation_sended_notice(event.event_id) # 6.4
-        if event_type == 'obligation_recieved' and (current_date == event.reminder_date or
-                                                    current_date > event.reminder_date):
-            obligation_recieved_notice(event.event_id) # ?
-        if event_type == 'obligation_money_requested' and (current_date == event.reminder_date or
+        # 6.4
+        if event.type == 'obligation_sended' and (current_date == event.reminder_date or
+                                                  current_date > event.reminder_date) and event.event_id not in list_event_id_obligation_sended:
+            list_event_id_obligation_sended.append(event.event_id)
+
+            obligation_sended_notice(event.event_id)
+
+        # 6.9
+        if event.type == 'obligation_recieved' and (current_date == event.reminder_date or
+                                                    current_date > event.reminder_date) and not event.sent:
+            list_event_id_obligation_recieved.append(event.event_id)
+
+            obligation_recieved_notice(event.event_id)
+            update_event(event.event_id, True)
+
+        if event.type == 'obligation_money_requested' and (current_date == event.reminder_date or
                                                            current_date > event.reminder_date):
             obligation_money_requested_notice(event.event_id)
-        if (event_type == 'reminder_in' or event_type == 'reminder_out') and \
-           (current_date == event.reminder_date or current_date > event.reminder_date):
-            reminder(event.event_id) # 6.3, 6.7, 6.8
+        if (event.type == 'reminder_in' or event.type == 'reminder_out') and \
+                (current_date == event.reminder_date or current_date > event.reminder_date):
+            reminder(event.event_id)  # 6.3, 6.7, 6.8
+
+        # 6.10
+        if event.type == 'obligation_sended' and (current_date - timedelta(days=5)) == event.reminder_date or (
+                current_date - timedelta(days=5)) > event.reminder_date and event.event_id not in list_event_id_for_6_10:
+            list_event_id_for_6_10.append(event.event_id)
+            reminder_for_6_10(event.event_id)
 
 
 while True:
@@ -64,4 +82,4 @@ while True:
         print(error)
     time.sleep(1)
 
-#bot.polling()
+# bot.polling()
