ab7e78935eff9826e7061c96ee67bb6b6623b51e
ArtemSmeta <smetatsc@yandex.ru>
2020-07-20 15:26:42 +0000

    Revert "fix global menu"
    This reverts commit 48915a12457f1bdea5f969ec6110408169c511fb

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def global_menu(message, dont_show_status=False):
     markup = types.ReplyKeyboardMarkup()
     # btn1 = types.KeyboardButton(text='Мой статус')
     btn2 = types.KeyboardButton(text='\U0001f4ca Транзакции')
-    btn3 = types.KeyboardButton(text='\U0001f527 Настройки')
+    btn3 = types.KeyboardButton(text='Настройки')
     btn4 = types.KeyboardButton(text='\U0001f465 Участники')
     # markup.row(btn1, btn2)
     markup.row(btn2)
@@ -130,11 +130,11 @@ def global_check(message):
     text = message.text
     # if text == 'Мой статус':
     #     status_menu(message)
-    if 'Транзакции' in text:
+    if text == 'Транзакции':
         transactions_menu(message)
-    elif 'Настройки' in text:
+    elif text == 'Настройки':
         configuration_menu(message)
-    elif 'Участники' in text:
+    elif text == 'Участники':
         members_menu(message)
 
 
