98b502f8c0f3f956356c3e88fbc815558d78ec6c
artemsmeta <smetatsc@yandex.ru>
2020-10-15 11:45:22 +0300

    edit text notifications

--- main.py
+++ main.py
@@ -230,7 +230,7 @@ def call_people_menu(message):
 
         status = user.status
         if status == "orange":
-            string_name = f'\n<a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {ORANGE_BALL} {left_sum} {HEART_RED}/{right_sum} {HELP}'
+            string_name = f'\n<a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {ORANGE_BALL} {left_sum} {HEART_RED} / {right_sum} {HELP}'
         elif "red" in status:
             string_name = f'\n<a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {RED_BALL} {right_sum} {HELP}'
         else:
@@ -1445,7 +1445,7 @@ def show_other_socium(message, user_id):
         if status == 'green':
             string_name = string_name + f'\n{user.exodus_id}. <a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {GREEN_BALL}'
         elif status == "orange":
-            string_name = string_name + f'\n{user.exodus_id}. <a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {ORANGE_BALL} {left_sum} {HEART_RED}/{right_sum} {HELP}'
+            string_name = string_name + f'\n{user.exodus_id}. <a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {ORANGE_BALL} {left_sum} {HEART_RED} / {right_sum} {HELP}'
         elif "red" in status:
             string_name = string_name + f'\n{user.exodus_id}. <a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {RED_BALL} {right_sum} {HELP}'
 
@@ -1502,7 +1502,7 @@ def show_my_socium(message):
         if status == 'green':
             string_name = string_name + f'\n{user.exodus_id}. <a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {GREEN_BALL}'
         elif status == "orange":
-            string_name = string_name + f'\n{user.exodus_id}. <a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {ORANGE_BALL} {left_sum} {HEART_RED}/{right_sum} {HELP}'
+            string_name = string_name + f'\n{user.exodus_id}. <a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {ORANGE_BALL} {left_sum} {HEART_RED} / {right_sum} {HELP}'
         elif "red" in status:
             string_name = string_name + f'\n{user.exodus_id}. <a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {RED_BALL} {right_sum} {HELP}'
         list_exodus_id_my_socium.append(user.exodus_id)
@@ -1982,7 +1982,7 @@ def intention_to_obligation(message):
 
     bot_text = f"Вы {HEART_RED}  {RIGHT_ARROW}  {HANDSHAKE} {intention.payment}  {RIGHT_ARROW}  {user_to.first_name} {user_to.last_name}\n\
 {user_to.first_name} {user_to.last_name} {status_to}\n\
-({left_sum}{HEART_RED}/{right_sum}{HELP} {LEFT_ARROW} {users_count} {PEOPLES})\n\
+({left_sum}{HEART_RED} / {right_sum}{HELP} {LEFT_ARROW} {users_count} {PEOPLES})\n\
 Ждите уведомления."
 
     bot.send_message(message.chat.id, bot_text)
@@ -1993,7 +1993,7 @@ def intention_to_obligation(message):
 
     bot_text_for_all = f"{user_from.first_name} {user_from.last_name} перевел свое {HEART_RED} в {HANDSHAKE} участнику {user_to.first_name} {user_to.last_name} на сумму: {intention.payment} {intention.currency}\n\
 {user_to.first_name} {user_to.last_name} {status_to}\n\
-({left_sum}{HEART_RED}/{right_sum}{HELP} {LEFT_ARROW} {users_count} {PEOPLES})"
+({left_sum}{HEART_RED} / {right_sum}{HELP} {LEFT_ARROW} {users_count} {PEOPLES})"
 
     for id in list_needy_id:
         bot.send_message(id, bot_text_for_all)
@@ -2611,7 +2611,7 @@ def for_me_obligation(message, reminder_call, intention_id):
 
     bot_text = f"{user_from.first_name} {user_from.last_name} {status_from} {RIGHT_ARROW} {HANDSHAKE} {intention.payment}\n\
 Вы: {status} \n\
-({left_sum}{HEART_RED}/{right_sum}{HELP})"
+({left_sum}{HEART_RED} / {right_sum}{HELP})"
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton(text='Запрос на исполнение')
@@ -3681,12 +3681,12 @@ def red_invitation_wizard_check(message, event_id=None):  # ------------------ T
     status = RED_BALL
     bot_text = f'Записано Ваше {HANDSHAKE} участнику {user.first_name} {user.last_name} на сумму {invitation_sum} {user.currency}\n\
 {user.first_name} {user.last_name}: {status} \n\
-({left_sum}{HEART_RED}/{right_sum}{HELP})'
+({left_sum}{HEART_RED} / {right_sum}{HELP})'
 
     # рассылка уведомлений моему кругу о том, что я начал кому то помогать, кроме того, кто запросил
     bot_text_for_all = f"{user_from.first_name} {user_from.last_name}  {RIGHT_ARROW}  {HANDSHAKE} {invitation_sum} {user.first_name} {user.last_name}\n\
 {user.first_name} {user.last_name}: {status} \n\
-({left_sum}{HEART_RED}/{right_sum}{HELP})"
+({left_sum}{HEART_RED} / {right_sum}{HELP})"
 
     for id in list_needy_id:
         bot.send_message(id, bot_text_for_all)
@@ -3697,7 +3697,7 @@ def red_invitation_wizard_check(message, event_id=None):  # ------------------ T
     # сообщение, получателю, что кто то записал обязательство в его пользу
     text_for_u = f"{user_from.first_name} {user_from.last_name} {status_from}  {RIGHT_ARROW}  {HANDSHAKE} {invitation_sum}\n\
 Ваш статус: {status} \n\
-({left_sum}{HEART_RED}/{right_sum}{HELP})"
+({left_sum}{HEART_RED} / {right_sum}{HELP})"
     bot.send_message(user.telegram_id, text_for_u)
 
     link = user.link
@@ -3759,7 +3759,7 @@ def red_invitation_wizard_check(message, event_id=None):  # ------------------ T
             right_sum = user.max_payments - already_payments_oblig if user.max_payments - already_payments_oblig > 0 else 0
 
             # сообщение Вам, что вы вернулись автоматически
-            text = f"Вы вернулись к {ORANGE_BALL}\n({left_sum}{HEART_RED}/{right_sum}{HELP})"
+            text = f"Вы вернулись к {ORANGE_BALL}\n({left_sum}{HEART_RED} / {right_sum}{HELP})"
             bot.send_message(user_id, text)
 
             global_menu(message)
@@ -4750,7 +4750,7 @@ def show_help_requisites(message):
         if status == 'green':
             bot_text = bot_text + f'\n{i}. <a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {GREEN_BALL}'
         elif status == "orange":
-            bot_text = bot_text + f'\n{i}. <a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {ORANGE_BALL} {left_sum} {HEART_RED}/{right_sum} {HELP}'
+            bot_text = bot_text + f'\n{i}. <a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {ORANGE_BALL} {left_sum} {HEART_RED} / {right_sum} {HELP}'
         elif "red" in status:
             bot_text = bot_text + f'\n{i}. <a href="tg://user?id={user.telegram_id}">{user.first_name} {user.last_name}</a> {RED_BALL} {right_sum} {HELP}'
 
